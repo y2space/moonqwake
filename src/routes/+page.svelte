217506a5e18e6c39714b02e1c9d242e1efba1d08
matteopolak <matteopolak@hotmail.com>
2023-10-07 15:25:44 -0400

    feat: draggable moon rotation

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,6 +3,7 @@
 	import * as THREE from 'three';
 
 	let renderCanvas: HTMLCanvasElement;
+	let moon: THREE.Mesh;
 
 	onMount(() => {
 		const scene = new THREE.Scene();
@@ -23,21 +24,48 @@
 		const texture = new THREE.TextureLoader().load('/moontexture.jpg');
 		const geometry = new THREE.SphereGeometry();
 		const material = new THREE.MeshBasicMaterial({ map: texture });
-		const cube = new THREE.Mesh(geometry, material);
+		moon = new THREE.Mesh(geometry, material);
 
-		scene.add(cube);
+		scene.add(moon);
 		camera.position.z = 5;
 
 		function animate() {
 			requestAnimationFrame(animate);
 			renderer.render(scene, camera);
+
+			moon.rotation.y += 0.001;
+			moon.rotation.x += 0.0005;
 		}
 
 		animate();
 	});
+
+	let dragStart = { x: 0, y: 0 };
+
+	function onMouseMove(event: MouseEvent) {
+		if (event.buttons === 1) {
+			const deltaRotationQuaternion = new THREE.Quaternion()
+				.setFromEuler(
+					new THREE.Euler(
+						((event.clientY - dragStart.y) * Math.PI) / 180,
+						((event.clientX - dragStart.x) * Math.PI) / 180,
+						0,
+						'XYZ'
+					)
+				)
+				.normalize();
+
+			moon.quaternion.multiplyQuaternions(
+				deltaRotationQuaternion,
+				moon.quaternion
+			);
+		}
+
+		dragStart = { x: event.clientX, y: event.clientY };
+	}
 </script>
 
-<canvas bind:this={renderCanvas} />
+<canvas bind:this={renderCanvas} on:mousemove={onMouseMove} />
 
 <div class="absolute top-2 left-2 bg-base-200 rounded-full p-3">
 	hello world
