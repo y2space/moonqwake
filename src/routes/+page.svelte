fc2da1b80fe628388cc44b86ed6f986221bdcb9a
Reuben D'Souza <reubenjds@gmail.com>
2023-10-07 18:20:59 -0400

    feat: changed spaces to tabs

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,151 +1,151 @@
 <script lang="ts">
-  import Controls from "$lib/components/Controls.svelte";
-  import { MONTHS } from "$lib/constants";
-  import { createScene } from "$lib/render";
+	import Controls from "$lib/components/Controls.svelte";
+	import { MONTHS } from "$lib/constants";
+	import { createScene } from "$lib/render";
 
-  import { onMount } from "svelte";
-  import * as THREE from "three";
+	import { onMount } from "svelte";
+	import * as THREE from "three";
 
-  let renderCanvas: HTMLCanvasElement;
-  let moon: THREE.Mesh;
-  let camera: THREE.PerspectiveCamera;
-  let light: THREE.DirectionalLight;
-  let skybox: THREE.Mesh;
+	let renderCanvas: HTMLCanvasElement;
+	let moon: THREE.Mesh;
+	let camera: THREE.PerspectiveCamera;
+	let light: THREE.DirectionalLight;
+	let skybox: THREE.Mesh;
 
-  let lightIntensity: number;
-  let currentTime = new Date();
-  let playTimeline = false;
-  let timelineValue = 0;
-  let lastPlayed: number;
+	let lightIntensity: number;
+	let currentTime = new Date();
+	let playTimeline = false;
+	let timelineValue = 0;
+	let lastPlayed: number;
 
-  $: if (light) light.intensity = lightIntensity / 20;
-  $: {
-    if (playTimeline) lastPlayed = setInterval(increaseTimeline, 1000);
-    else clearInterval(lastPlayed);
-  }
+	$: if (light) light.intensity = lightIntensity / 20;
+	$: {
+		if (playTimeline) lastPlayed = setInterval(increaseTimeline, 1000);
+		else clearInterval(lastPlayed);
+	}
 
-  function increaseTimeline() {
-    return (timelineValue += 1);
-  }
+	function increaseTimeline() {
+		return (timelineValue += 1);
+	}
 
-  onMount(() => {
-    const scene = new THREE.Scene();
-    const renderer = new THREE.WebGL1Renderer({
-      canvas: renderCanvas,
-    });
+	onMount(() => {
+		const scene = new THREE.Scene();
+		const renderer = new THREE.WebGL1Renderer({
+			canvas: renderCanvas,
+		});
 
-    renderer.setSize(window.innerWidth, window.innerHeight);
-    document.body.appendChild(renderer.domElement);
+		renderer.setSize(window.innerWidth, window.innerHeight);
+		document.body.appendChild(renderer.domElement);
 
-    camera = new THREE.PerspectiveCamera(
-      75,
-      window.innerWidth / window.innerHeight,
-      0.1,
-      1000
-    );
+		camera = new THREE.PerspectiveCamera(
+			75,
+			window.innerWidth / window.innerHeight,
+			0.1,
+			1000
+		);
 
-    const models = createScene(scene);
+		const models = createScene(scene);
 
-    moon = models.moon;
-    light = models.light;
-    camera.position.z = 3;
+		moon = models.moon;
+		light = models.light;
+		camera.position.z = 3;
 
-    function animate() {
-      requestAnimationFrame(animate);
-      renderer.render(scene, camera);
+		function animate() {
+			requestAnimationFrame(animate);
+			renderer.render(scene, camera);
 
-      if (!usedManual) {
-        moon.rotation.y += 0.001;
-        moon.rotation.x += 0.0005;
-      }
+			if (!usedManual) {
+				moon.rotation.y += 0.001;
+				moon.rotation.x += 0.0005;
+			}
 
-      models.skybox.rotation.y += 0.001;
-      models.skybox.rotation.x += 0.0005;
-    }
+			models.skybox.rotation.y += 0.001;
+			models.skybox.rotation.x += 0.0005;
+		}
 
-    animate();
-  });
+		animate();
+	});
 
-  let usedManual = false;
-  let dragStart = { x: 0, y: 0 };
-  let skyboxOffset = { x: 0, y: 0 };
+	let usedManual = false;
+	let dragStart = { x: 0, y: 0 };
+	let skyboxOffset = { x: 0, y: 0 };
 
-  function onMouseScroll(event: WheelEvent) {
-    event.preventDefault();
-    const zoom = Math.min(
-      8,
-      Math.max(1.5, camera.position.z + (Math.sign(event.deltaY) * 70) / 1000)
-    );
-    camera.position.z = zoom;
-    console.log(event.deltaY);
-  }
+	function onMouseScroll(event: WheelEvent) {
+		event.preventDefault();
+		const zoom = Math.min(
+			8,
+			Math.max(1.5, camera.position.z + (Math.sign(event.deltaY) * 70) / 1000)
+		);
+		camera.position.z = zoom;
+		console.log(event.deltaY);
+	}
 
-  function onMouseMove(event: MouseEvent) {
-    function clamp(x: number, a: number, b: number) {
-      return Math.min(Math.max(x, b), a);
-    }
+	function onMouseMove(event: MouseEvent) {
+		function clamp(x: number, a: number, b: number) {
+			return Math.min(Math.max(x, b), a);
+		}
 
-    const parralaxAmount = 10000;
-    const maxAmount = 0.1;
-    const deltaY = (window.innerHeight / 2 - event.clientY) / parralaxAmount;
-    const deltaX = (window.innerWidth / 2 - event.clientX) / parralaxAmount;
+		const parralaxAmount = 10000;
+		const maxAmount = 0.1;
+		const deltaY = (window.innerHeight / 2 - event.clientY) / parralaxAmount;
+		const deltaX = (window.innerWidth / 2 - event.clientX) / parralaxAmount;
 
-    skybox.rotation.y = -clamp(skyboxOffset.x + deltaX, maxAmount, -maxAmount);
-    skybox.rotation.x = -clamp(skyboxOffset.y + deltaY, maxAmount, -maxAmount);
+		skybox.rotation.y = -clamp(skyboxOffset.x + deltaX, maxAmount, -maxAmount);
+		skybox.rotation.x = -clamp(skyboxOffset.y + deltaY, maxAmount, -maxAmount);
 
-    if (event.buttons === 1) {
-      const deltaRotationQuaternion = new THREE.Quaternion()
-        .setFromEuler(
-          new THREE.Euler(
-            ((event.clientY - dragStart.y) * Math.PI) / 180,
-            ((event.clientX - dragStart.x) * Math.PI) / 180,
-            0,
-            "XYZ"
-          )
-        )
-        .normalize();
+		if (event.buttons === 1) {
+			const deltaRotationQuaternion = new THREE.Quaternion()
+				.setFromEuler(
+					new THREE.Euler(
+						((event.clientY - dragStart.y) * Math.PI) / 180,
+						((event.clientX - dragStart.x) * Math.PI) / 180,
+						0,
+						"XYZ"
+					)
+				)
+				.normalize();
 
-      moon.quaternion.multiplyQuaternions(
-        deltaRotationQuaternion,
-        moon.quaternion
-      );
+			moon.quaternion.multiplyQuaternions(
+				deltaRotationQuaternion,
+				moon.quaternion
+			);
 
-      usedManual = true;
-    } else {
-      usedManual = false;
-    }
+			usedManual = true;
+		} else {
+			usedManual = false;
+		}
 
-    dragStart = { x: event.clientX, y: event.clientY };
-  }
+		dragStart = { x: event.clientX, y: event.clientY };
+	}
 </script>
 
 <canvas
-  bind:this={renderCanvas}
-  on:mousemove={onMouseMove}
-  on:wheel={onMouseScroll}
+	bind:this={renderCanvas}
+	on:mousemove={onMouseMove}
+	on:wheel={onMouseScroll}
 />
 
 <Controls bind:lightIntensity />
 
 <div class="absolute bottom-2 right-2 text-white rounded-full p-3">
-  {MONTHS[currentTime.getMonth()]}
-  {currentTime.getDate()}, {currentTime.getFullYear()}
+	{MONTHS[currentTime.getMonth()]}
+	{currentTime.getDate()}, {currentTime.getFullYear()}
 </div>
 
 <div
-  class="absolute text-white rounded-full p-3 left-0 right-0 bottom-24 grid w-full"
+	class="absolute text-white rounded-full p-3 left-0 right-0 bottom-24 grid w-full"
 >
-  <input
-    type="range"
-    min="0"
-    max="100"
-    bind:value={timelineValue}
-    class="range w-full mx-auto"
-  />
+	<input
+		type="range"
+		min="0"
+		max="100"
+		bind:value={timelineValue}
+		class="range w-full mx-auto"
+	/>
 
-  <input
-    type="checkbox"
-    class="toggle toggle-success"
-    bind:checked={playTimeline}
-  />
+	<input
+		type="checkbox"
+		class="toggle toggle-success"
+		bind:checked={playTimeline}
+	/>
 </div>
