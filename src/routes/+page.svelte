2867ccfd8f4ca72b270df5b527093cbbedae8507
Vasil <vasiltopalovic@outlook.com>
2023-10-07 14:56:41 -0400

    feat: texture moon

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,16 +5,18 @@
 
     
     onMount(() => {
-
+        
         const scene = new THREE.Scene();
         const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
         const renderer = new THREE.WebGL1Renderer();
+        const loader = new THREE.TextureLoader();
+        let texture = new THREE.TextureLoader().load('/moontexture.jpg');
 
         renderer.setSize( window.innerWidth, window.innerHeight );
         document.body.appendChild( renderer.domElement ); 
 
         const geometry = new THREE.SphereGeometry();
-        const material = new THREE.MeshBasicMaterial({ color: 0x00ff00});
+        const material = new THREE.MeshBasicMaterial({map: texture});
         const cube = new THREE.Mesh(geometry, material);
         scene.add(cube);
 
@@ -31,13 +33,6 @@
         
     })
 
-
-    
-    
-    
 </script>
 
-<h1>Welcome to SvelteKit</h1>
-<p>
-	Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
-</p>
+
