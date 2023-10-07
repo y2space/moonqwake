0525301dab429fed7adab8bc9e9ca175efcf9201
Vasil <vasiltopalovic@outlook.com>
2023-10-07 16:19:29 -0400

    feat: skybox

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -21,19 +21,27 @@
 		renderer.setSize(window.innerWidth, window.innerHeight);
 		document.body.appendChild(renderer.domElement);
 
-		const texture = new THREE.TextureLoader().load('/moontexture.jpg');
-        const normalMap = new THREE.TextureLoader().load('moonnormal.jpg');
 
-		const geometry = new THREE.SphereGeometry();
-		const material = new THREE.MeshStandardMaterial({ map: texture, normalMap: normalMap});
-		moon = new THREE.Mesh(geometry, material);
+        const skyboxTexture = new THREE.TextureLoader().load('/stars.jpg');
+        const skyboxGeometry = new THREE.SphereGeometry(10);
+        const skyboxMaterial = new THREE.MeshBasicMaterial({map: skyboxTexture});
+        const skybox = new THREE.Mesh(skyboxGeometry, skyboxMaterial);
+        skybox.material.side = THREE.DoubleSide;
+        scene.add(skybox);
+
+
+		const moonTexture = new THREE.TextureLoader().load('/moontexture.jpg');
+        const moonNormalMap = new THREE.TextureLoader().load('/moonnormal.jpg');
+		const moonGeometry = new THREE.SphereGeometry();
+		const moonMaterial = new THREE.MeshStandardMaterial({ map: moonTexture, normalMap: moonNormalMap});
+		moon = new THREE.Mesh(moonGeometry, moonMaterial);
+		scene.add(moon);
 
         const light = new THREE.DirectionalLight(0xffffff, 1.4);
         light.position.set(0, 0, 1);
         scene.add(light);
 
 
-		scene.add(moon);
 
 		camera.position.z = 5;
 
@@ -43,6 +51,12 @@
 
 			moon.rotation.y += 0.001;
 			moon.rotation.x += 0.0005;
+
+			skybox.rotation.y += 0.001;
+			skybox.rotation.x += 0.0005;
+
+
+
 		}
 
 		animate();
