2d3fe31784cda2a04e3624ea893f1e9c09e9882d
Reuben D'Souza <reubenjds@gmail.com>
2023-10-08 14:04:48 -0400

    feat: added table to show all quakes

---
+++ src/lib/components/DataTable.svelte
@@ -0,0 +1,31 @@
+<script>
+	import quakes from "$lib/quakedata";
+</script>
+
+<div class="overflow-x-auto">
+	<table class="table table-xs table-pin-rows table-pin-cols">
+		<thead>
+			<tr>
+				<th />
+				<td>Date</td>
+				<td>Name</td>
+			</tr>
+		</thead>
+		<tbody>
+			{#each quakes as quake, index}
+				<tr>
+					<th>{index + 1}</th>
+					<th>{quake.date}</th>
+					<td>{quake.type}</td>
+				</tr>
+			{/each}
+		</tbody>
+		<tfoot>
+			<tr>
+				<th />
+				<td>Date</td>
+				<td>Name</td>
+			</tr>
+		</tfoot>
+	</table>
+</div>
